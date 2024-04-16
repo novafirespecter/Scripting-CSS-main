84913dc61cbf865e42b686cd25fc11a7445a89f9
novafirespecter <6000144@sae.edu.za>
2024-04-16 09:48:28 +0200

    first commit

---
+++ Scripting-CSS-main-main-main/JS/script.js
@@ -0,0 +1,52 @@
+// let myEmail = "6000144@sae.edu.za";
+// let password = "Gocheckthecode3725";
+// let x = String(360 / 8);
+// let num1 = 4;
+// let num2 = 2;
+// let quotient = num1 / num2;
+// let quotientString = quotient.toString();
+
+// const tempText = `Insert email/username and password: ${myEmail}, ${password}`;
+// const gtaProtag = ["Michael","Trevor","Franklin"];
+
+// gtaProtag.push("Lester","Lemar");
+
+// console.log(tempText);
+// console.log(gtaProtag);
+// console.log(x);
+// console.log(quotientString);
+
+// function concatenateStrings(string1,string2)
+// {
+//     return string1 + string2;
+// }
+
+// const result = concatenateStrings('Crazy ', 'Frenzy')
+// console.log(result);
+
+
+//Activity 1
+
+function createGreeting2 (name) 
+{
+    return `Hello , ${name} !` ;
+}
+
+console.log(createGreeting2("Lucas")) ;
+
+// Activity 2
+
+number1  = 1;
+number2 = 2;
+
+function addAndPrint(number1 , number2 )
+{
+    let sum = number1 + number2;
+    console.log("The sum of" , number1, "and" , number2 , "is", sum) 
+}
+addAndPrint(5 , 10);
+
+//Activity 3
+
+multiplyFunction = (num3 , num4) => num3 * num4;
+console.log(multiplyFunction(3,4));
