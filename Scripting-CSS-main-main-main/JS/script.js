72b39b6c51dd3995478674345c73b776dfc925bd
novafirespecter <6000144@sae.edu.za>
2024-05-07 10:44:02 +0200

    Folder renamed and new files

--- Scripting-CSS-main-main-main/JS/script.js
+++ Scripting-CSS-main-main-main/JS/script.js
@@ -80,6 +80,6 @@
   // });
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function() {
+particlesJS.load('particles-js', 'PlugIns/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
